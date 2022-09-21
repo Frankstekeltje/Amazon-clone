b01988328f0ba5c48dfc342803faa8ec8e1dea36
frankstekeltje <frankstekelenburg@ziggo.nl>
2022-09-21 15:53:13 +0200

    basket right added to the program
    working with react context/ Look at redux as a store manager

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,9 +8,10 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Routes>
-          <Route path='/' element={Array(<Header />, <Home />)} />
-          <Route path='/checkout' element={<Header/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
         </Routes>
       </div>
     </Router>
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='header'>
-        <img className='header__logo' src="https://images-eu.ssl-images-amazon.com/images/G/37/gno/images/general/backup-logo_blue._CB481607158_.png" />
-
+          <Link to={'/'}>
+              <img className='header__logo' src="https://images-eu.ssl-images-amazon.com/images/G/37/gno/images/general/backup-logo_blue._CB481607158_.png" />
+          </Link>
         <div className='header__search'>
             <input className='header__searchInput' type="text" />
             {/* Logo */}
@@ -42,12 +47,14 @@ function Header() {
                 </span>
            </div>
 
-           <div className='header__optionBasket'>
-            <ShoppingBasketIcon />
-            <span className='header__optionLineTwo header__basketCount'>
-                0
-            </span>
-           </div>
+              <Link to={'/checkout'}>
+                  <div className='header__optionBasket'>
+                      <ShoppingBasketIcon />
+                      <span className='header__optionLineTwo header__basketCount'>
+                          {basket?.length}
+                      </span>
+                  </div>
+              </Link>
         </div>
     </div>
   )
