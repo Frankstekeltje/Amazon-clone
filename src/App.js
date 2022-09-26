7656e90516f87b34ef37f18b1008db6a80c0e314
frankstekeltje <frankstekelenburg@ziggo.nl>
2022-09-26 20:19:09 +0200

    Working on login page front-end
    Component added but just working on front-end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -8,10 +9,10 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={Array(<Header />, <Home />)} />
+          <Route path='/checkout' element={Array(<Header />, <Checkout />)} />
         </Routes>
       </div>
     </Router>
--- src/Header.js
+++ src/Header.js
@@ -20,14 +20,16 @@ function Header() {
         </div>
 
         <div className='header__nav'>
-           <div className='header__option'>
-                <span className='header__optionLineOne'>
-                    Hello Guest
-                </span>
-                <span className='header__optionLineTwo'>
-                    Sign In
-                </span>
-           </div>
+              <Link className='login__link' to={'/login'}>
+                  <div className='header__option'>
+                      <span className='header__optionLineOne'>
+                          Hello Guest
+                      </span>
+                      <span className='header__optionLineTwo'>
+                          Sign In
+                      </span>
+                  </div>
+              </Link>
 
            <div className='header__option'>
                 <span className='header__optionLineOne'>
