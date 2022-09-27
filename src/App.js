09abcbaa27917b76d0c80d3c148dd8eefe58595d
Frank Stekelenburg <frankstekelenburg@ziggo.nl>
2022-09-27 21:34:15 +0200

    added login with firebase

--- src/App.js
+++ src/App.js
@@ -4,8 +4,35 @@ import Checkout from './Checkout';
 import Login from './Login';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{ }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //Will only run once when the app component loads...
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>> ', authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        //the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from './firebase';
+import userEvent from '@testing-library/user-event';
 
 function Login() {
+    const history = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password).then((auth) => {
+            history('/')
+        }).catch(error => alert(error.message));
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password).then((auth) => {
+            //it successfully created a new use with email and password
+            console.log(auth);
+            if (auth) {
+                history('/')
+            }
+        }).catch(error => alert(error.message));
+    }
+
     return (
         <div className='login'>
             <Link to='/'>
@@ -11,13 +37,14 @@ function Login() {
 
             <div className='login__container'>
                 <h1>Sign-in</h1>
+
                 <form action="">
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
 
-                    <button className='login__signInButton'>Sign in</button>
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign in</button>
                 </form>
 
                 <p>
@@ -27,7 +54,7 @@ function Login() {
                     and our Interest Based Ads Notice.
                 </p>
 
-                <button className='login__registerButton'>Create your Amazon account</button>
+                <button onClick={register} className='login__registerButton'>Create your Amazon account</button>
             </div>
         </div>
     )
